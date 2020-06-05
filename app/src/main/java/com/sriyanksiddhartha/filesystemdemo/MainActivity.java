03af85724c880b6242f0c76e0ffc2843b6129393
SinaSystem <sinasystem47@gmail.com>
2020-06-05 12:30:11 +0430

    Initial commit

---
+++ app/src/main/java/com/sriyanksiddhartha/filesystemdemo/MainActivity.java
@@ -0,0 +1,75 @@
+package com.sriyanksiddhartha.filesystemdemo;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.sriyanksiddhartha.filesystemdemo.storageoptions.CacheStorageDemo;
+import com.sriyanksiddhartha.filesystemdemo.storageoptions.ExternalStorageDemo;
+
+/**
+ * Author: Sriyank Siddhartha
+ * <p>
+ * Module3: Working with External Storage
+ * <p>
+ * "AFTER" Project
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 28);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        switch (requestCode) {
+
+            case 28: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("MainActivity", "Permission Granted");
+                } else { // Permission Denied : Disable the functionality related to the permission
+                    Log.i("MainActivity", "Permission Denied");
+                }
+            }
+        }
+    }
+
+
+    public void openInternalStorageDemoActivity(View view) {
+
+        //Intent intent = new Intent(this, InternalStorageDemo.class);
+        //	startActivity(intent);
+    }
+
+    public void openCacheStorageDemoActivity(View view) {
+
+        //	Intent intent = new Intent(this, CacheStorageDemo.class);
+        //startActivity(intent);
+    }
+
+    public void openExternalStorageDemoActivity(View view) {
+
+        Intent intent = new Intent(this, ExternalStorageDemo.class);
+        startActivity(intent);
+    }
+}
+
